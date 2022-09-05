d96ffeb0d0f50dc456249529a376d518af5b9892
Jefferson Garcia <ekball007@outlook.com>
2022-09-05 06:09:04 -0300

    par&impar

--- Python/while/while.py
+++ Python/while/while.py
@@ -1,9 +1,18 @@
+# duas formas iguais para criar um programa que imprima o mesmos
+#resultado porem a diferença entre eles.
+#For quando eu sei o limite dos valores a serem executados
+
 #for c in range(1,10):
  #   print(c)
 #print("Fim")
+
+#=====================================#
 
 c = 1 
 while c != 10:
     print(c)
     c += 1
 print("Fim")
+
+#While serve para o mesmo proposito mas é eficiente quando.
+#quando tenho como saber a quantidade de dados entrará.
